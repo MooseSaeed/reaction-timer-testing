48dbf5ee2891b19ded0bb1cc14d89bc1a01e0eae
Mostafa Said <94674993+MooseSaeed@users.noreply.github.com>
2022-01-27 16:40:56 +0200

    Getting started with App and block

---
+++ src/components/Block.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="block">Click Me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  background: #0faf87;
+  color: #fff;
+  border-radius: 20px;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
